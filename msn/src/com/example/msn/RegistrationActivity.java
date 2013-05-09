f024c86d89b00cf5d24ab140bbb906b7e1275eaf
ashish chopra <ashishchopra0308@gmail.com>
2013-05-09 17:41:13 +0530

    changed all popup images

--- msn/src/com/example/msn/RegistrationActivity.java
+++ msn/src/com/example/msn/RegistrationActivity.java
@@ -1,5 +1,8 @@
 package com.example.msn;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
@@ -8,6 +11,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class RegistrationActivity extends Activity {
 	ImageView submitBtn;
@@ -15,6 +19,7 @@ public class RegistrationActivity extends Activity {
 	EditText email;
 	EditText city;
 	EditText contact;
+	Toast toast;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -24,24 +29,67 @@ public class RegistrationActivity extends Activity {
 		city = (EditText) findViewById(R.id.city);
 		email = (EditText) findViewById(R.id.email);
 		contact = (EditText) findViewById(R.id.phone);
-		
-		UserInfo.setName(name.getText().toString());
-		UserInfo.setEmail(email.getText().toString());
-		UserInfo.setCity(city.getText().toString());
-		UserInfo.setContact(contact.getText().toString());
-		
+	
 		submitBtn.setOnClickListener(new View.OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				Intent uploadIntent = new Intent("com.example.msn.UPLOADGALLERY");
-				startActivity(uploadIntent);
+
+				String uname = name.getText().toString();
+				String uemail = email.getText().toString();
+				String ucity = city.getText().toString();
+				String ucontact = contact.getText().toString();
+				System.out.println(uname.length());
+				if(uname.length() == 0) {
+					toast = Toast.makeText(getBaseContext(), "Please Enter Name!!", 100);
+					toast.show();
+				}
+				else if(uemail.length() == 0) {
+					toast = Toast.makeText(getBaseContext(), "Please Enter Email!!", 100);
+					toast.show();
+				}
+				else if(ucity.length() == 0) {
+					toast = Toast.makeText(getBaseContext(), "Please Enter City!!", 100);
+					toast.show();
+				}
+				else if(ucontact.length() == 0) {
+					toast = Toast.makeText(getBaseContext(), "Please Enter Mobile Number!!", 100);
+					toast.show();
+				}
+				else if(!chkEmail(uemail)) {
+					toast = Toast.makeText(getBaseContext(), "Please Enter Valid Email!!", 100);
+					toast.show();
+				}
+				else if(ucontact.length() != 10) {
+					toast = Toast.makeText(getBaseContext(), "Please Enter Valid 10 digit Mobile Number!!", 100);
+					toast.show();
+				}
+				else {
+					UserInfo.setName(uname);
+					UserInfo.setEmail(uemail);
+					UserInfo.setCity(ucity);
+					UserInfo.setContact(ucontact);
+					Intent uploadIntent = new Intent("com.example.msn.UPLOADGALLERY");
+					startActivity(uploadIntent);
+				}
 			}
 		});
 	}
 
-	@Override
+	public boolean chkEmail(String uemail) {
+		String regEx = "\\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}\\b";
+		Pattern p = Pattern.compile(regEx);
+		Matcher m = p.matcher(uemail);
+		if(m.find()) 
+		 {
+		  return true; }
+		 else
+		 {
+		  return false;
+		 }
+	}
+	
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.registration, menu);
