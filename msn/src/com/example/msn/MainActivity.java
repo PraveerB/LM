168173100c8b47e244a5fb17e26d00097aa57b95
Ashish Chopra <ashish@ashish-chopra.(none)>
2013-05-17 20:35:41 +0530

    main activity changes

--- msn/src/com/example/msn/MainActivity.java
+++ msn/src/com/example/msn/MainActivity.java
@@ -11,10 +11,21 @@ import android.widget.ImageView;
 public class MainActivity extends Activity  {
 	
 	ImageView participateBtn;
+	ImageView gallery;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        gallery = (ImageView) findViewById(R.id.gotogGallery);
+        gallery.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent gallery = new Intent("com.example.msn.GALLERY");
+				startActivity(gallery);
+			}
+		});
         participateBtn = (ImageView) findViewById(R.id.participateBtn);
         participateBtn.setOnClickListener(new OnClickListener() {
         	@Override
